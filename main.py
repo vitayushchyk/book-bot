b39c6e7682ae5cedaa35ce8d7051a99966ac1823
vitayushchyk <vitay2715@gmail.com>
2025-03-18 14:59:13 +0200

    Refactor bot structure and improve book search flow
    Reorganized project structure for better modularity and maintainability. Migrated handlers to a conversational flow, added improved user feedback, and revised the book search logic for enhanced clarity and error handling. Removed unused or redundant code to streamline functionality.

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import atexit
 import logging
 
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
-from core.config import settings
-from core.handlers.start_hendler import start
-from core.sens import Sens
-from driver.chrome_driver import get_driver
+from bot.book_shop.sens import Sens
+from bot.core.config import settings
+from bot.driver.chrome_driver import get_driver
+from bot.handlers.sens_handler import (
+    NAME_BOOK,
+    book_name_handle,
+    cancel_handler,
+    start_search_book_handler,
+)
+from bot.handlers.start_handler import start
 
 driver = get_driver()
-
-
-def close_driver():
-    logging.info("quit ChromeDriver...")
-    driver.quit()
-
-
-atexit.register(close_driver)
-
 logging.basicConfig()
 logging.getLogger().setLevel(settings.get_log_level())
 
@@ -25,25 +27,42 @@ logging.getLogger().setLevel(settings.get_log_level())
 def get_app():
     logging.info("Initializing the bot...")
     try:
-        sens_url = "https://sens.in.ua/kataloh/search/?q="
+        sens_url = settings.search_url_sens
         sens = Sens(driver, sens_url)
 
         app = ApplicationBuilder().token(settings.bot_token).build()
 
         app.add_handler(CommandHandler("start", start))
-        app.add_handler(
-            CommandHandler(
-                "findbook",
-                lambda update, context: sens.get_book_handler(update, context),
-            )
+
+        find_book_handler = ConversationHandler(
+            entry_points=[
+                CommandHandler(
+                    "findbook",
+                    lambda update, context: start_search_book_handler(
+                        sens, update, context
+                    ),
+                ),
+            ],
+            states={
+                NAME_BOOK: [
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND,
+                        lambda update, context: book_name_handle(sens, update, context),
+                    )
+                ],
+            },
+            fallbacks=[
+                CommandHandler("cancel", cancel_handler),
+            ],
         )
+        app.add_handler(find_book_handler)
 
         logging.info("Bot initialized successfully.")
         logging.info("Bot is running. Press Ctrl+C to stop.")
         app.run_polling()
 
     except Exception as e:
-        logging.error("Error occurred while launching the bot: %s", e)
+        logging.error(f"Error occurred while launching the bot: {e}", exc_info=True)
 
 
 if __name__ == "__main__":
