d01598ae01a1c665b8369d4ac3c3b8bd837a7fd8
vitayushchyk <vitay2715@gmail.com>
2025-04-06 21:24:02 +0300

    Add support for Readeat book store integration.
    Introduced functionality to fetch and display book details from the Readeat store. Updated configuration.

--- bot/book_shop/readeat.py
+++ bot/book_shop/readeat.py
@@ -1,6 +1,32 @@
+import logging
+
+from selenium.webdriver.common.by import By
+
 from bot.book_shop.base_shop import BaseShop
+from bot.utils.book_details import get_book_details, get_book_details_from_element
 
 
 class Readeat(BaseShop):
-    async def get_book(self, book_name: str) -> list:
-        pass
+    async def get_book(self, book_name):
+        search_url_readeat = f"{self.baseurl}{book_name}"
+
+        self.driver.get(search_url_readeat)
+
+        try:
+            book_elements = self.driver.find_elements(By.CSS_SELECTOR, ".card-img-top")
+            matching_books = []
+
+            for book_element in book_elements:
+
+                book_details = await get_book_details_from_element(
+                    book_element, self.driver
+                )
+                if book_details:
+                    raw_book_data = await get_book_details(book_details, "readeat")
+                    if raw_book_data:
+                        matching_books.append(raw_book_data)
+
+            return matching_books
+        except Exception as e:
+            logging.error(f"Error loading books from readeat: {str(e)}")
+            return []
--- bot/core/config.py
+++ bot/core/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     selenium_url: str
     selenium_status_url: str
     search_url_yakaboo: str
+    search_url_readeat: str
     redis_host: str
     redis_port: int
 
